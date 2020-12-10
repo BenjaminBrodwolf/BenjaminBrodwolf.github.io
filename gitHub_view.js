b54a05c5927be8833eefc145eb82e3e7cbe4e750
Benjamin Brodwolf <benjamin.brodolf@students.fhnw.ch>
2020-12-10 13:11:22 +0100

    mobile fix

--- gitHub_view.js
+++ gitHub_view.js
@@ -144,15 +144,15 @@ const skills = {
     "Unity3D": "40",
     "Arduino": "34",
 }
-const mql = window.matchMedia('(max-width: 600px)');
 
 const boxes = []
 const displayExperiences = experiences => {
-
+    const mql = window.matchMedia('(max-width: 800px)').matches;
+    
     let exp = ""
     //          [edu, exp, exp, travel, exp, exp, edu, exp]
-    const tops = mql ? [0, 1, 2, 3, 4, 5, 6, 8] : [0, 1, 2, 2.7, 3, 4, 0, 2]
-    const lefts = mql ? [0, 3, 6, 15, 9, 13, -3, 6] : [-5, 5, 5, -15, 5, 5, 30, 35]
+    const tops = mql ? [0, 1, 2, 3, 4, 5, 6, 8] : [0, 1, 2, 3, 4, 5, 0, 2]
+    const lefts = mql ? [0, 3, 6, 15, 9, 13, -3, 6] : [-5, 5, 5, -7, 5, 5, 30, 35]
 
 
     const animationDelay = i => (i + 5) / 2;
@@ -189,7 +189,7 @@ const displayExperiences = experiences => {
         adjustLine(getElements(boxes[i], boxes[i + 1]), animationDelay(i))
     }
 
-    const height =  distanceToTop(getElement(boxes[boxes.length - 3]))
+    const height = distanceToTop(getElement(boxes[boxes.length - 5]))
     document.documentElement.style
         .setProperty("--experience-height", height + "px");
 }
@@ -261,7 +261,7 @@ const displaySites = myWebsites => {
 
 let recomposeToggle = true;
 window.addEventListener("resize", () => {
-    // console.log(mql.matches)
+    console.log(mql.matches)
 
 
     if (boxes) {
@@ -271,7 +271,7 @@ window.addEventListener("resize", () => {
             adjustLine(getElements(boxes[i], boxes[i + 1]))
         }
 
-        const height = distanceToTop(getElement(boxes[boxes.length - 3]))
+        const height = distanceToTop(getElement(boxes[boxes.length]))
         document.documentElement.style
             .setProperty("--experience-height", height + "px");
     }
